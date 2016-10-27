d742031a91e847a35ec96e12f4417108f1586465
Yamid Pico <ypicoleal@gmail.com>
2016-10-26 23:47:52 -0500

    Fomulario planilla

--- app/src/main/java/co/com/exile/piscix/PlanillaActivity.java
+++ app/src/main/java/co/com/exile/piscix/PlanillaActivity.java
@@ -1,10 +1,9 @@
 package co.com.exile.piscix;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 
 public class PlanillaActivity extends AppCompatActivity {
@@ -15,15 +14,18 @@ public class PlanillaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_planilla);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                finish();
             }
         });
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.planilla, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 }
