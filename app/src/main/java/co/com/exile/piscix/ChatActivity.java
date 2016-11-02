81054467909c58b0b81eb3b3dd08de3d0d1cd08f
Yamid Pico <ypicoleal@gmail.com>
2016-11-02 17:34:26 -0500

    Callapsed toolbar para mostrar detalles en el chat

--- app/src/main/java/co/com/exile/piscix/ChatActivity.java
+++ app/src/main/java/co/com/exile/piscix/ChatActivity.java
@@ -1,25 +1,18 @@
 package co.com.exile.piscix;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.softw4re.views.InfiniteListAdapter;
-import com.softw4re.views.InfiniteListView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -31,7 +24,7 @@ import co.com.exile.piscix.models.Mensaje;
 
 public class ChatActivity extends AppCompatActivity {
 
-    private InfiniteListView infiniteListView;
+    private RecyclerView infiniteListView;
     private ArrayList<Mensaje> itemList;
 
     private int reporte;
@@ -49,103 +42,23 @@ public class ChatActivity extends AppCompatActivity {
     }
 
     void setInfiniteList() {
-        infiniteListView = (InfiniteListView) findViewById(R.id.content_chat);
-
         itemList = new ArrayList<>();
-        InfiniteListAdapter adapter = new InfiniteListAdapter<Mensaje>(this, R.layout.chat_bubble, itemList) {
-            @Override
-            public void onNewLoadRequired() {
-                getReportes();
-            }
-
-            @Override
-            public void onRefresh() {
-                infiniteListView.clearList();
-                getReportes();
-            }
-
-            @Override
-            public void onItemClick(int i) {
-            }
-
-            @Override
-            public void onItemLongClick(int i) {
-                Toast.makeText(getContext(), "long click", Toast.LENGTH_LONG).show();
-            }
-
-            @NonNull
-            @Override
-            public View getView(final int position, View convertView, @NonNull ViewGroup parent) {
-
-                ViewHolder holder;
-
-                if (convertView == null) {
-                    convertView = getLayoutInflater().inflate(R.layout.chat_bubble, parent, false);
-                    holder = new ViewHolder();
-                    holder.user = (TextView) convertView.findViewById(R.id.user);
-                    holder.mensaje = (TextView) convertView.findViewById(R.id.message);
-                    holder.bubble = (CardView) convertView.findViewById(R.id.bubble);
-
-                    convertView.setTag(holder);
-
-                } else {
-                    holder = (ViewHolder) convertView.getTag();
-                }
-
-                final Mensaje mensaje = itemList.get(position);
-                if (mensaje != null) {
-                    float scale = getApplicationContext().getResources().getDisplayMetrics().density;
-                    final int max_margin = (int) (72 * scale + 0.5f);
-                    final int min_margin = (int) (16 * scale + 0.5f);
-                    int top_margin = (int) (16 * scale + 0.5f);
-                    int bottom_margin = 0;
-                    RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-
-                    if (position > 0 && itemList.get(position - 1).getUser().equals(mensaje.getUser())) {
-                        top_margin = (int) (2 * scale + 0.5f);
-                    }
-
-                    if ((position + 1) >= itemList.size()) {
-                        bottom_margin = (int) (16 * scale + 0.5f);
-                    }
-
-                    holder.mensaje.setText(mensaje.getMensaje());
-                    if (!mensaje.isTu()) {
-                        holder.mensaje.setTextColor(ContextCompat.getColor(this.getContext(), R.color.white));
-                        holder.user.setText(mensaje.getUser());
-                        if (position > 0 && itemList.get(position - 1).getUser().equals(mensaje.getUser())) {
-                            holder.user.setVisibility(View.GONE);
-                        } else {
-                            holder.user.setVisibility(View.VISIBLE);
-                        }
-                        holder.bubble.setCardBackgroundColor(ContextCompat.getColor(this.getContext(), R.color.colorPrimary));
-                        layoutParams.setMargins(min_margin, top_margin, max_margin, bottom_margin);
-                    } else {
-                        holder.user.setVisibility(View.GONE);
-                        holder.bubble.setCardBackgroundColor(ContextCompat.getColor(this.getContext(), R.color.white));
-                        holder.mensaje.setTextColor(ContextCompat.getColor(this.getContext(), R.color.grey));
-                        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_END);
-                        layoutParams.setMargins(max_margin, top_margin, min_margin, bottom_margin);
-                    }
-                    holder.bubble.setLayoutParams(layoutParams);
-                }
-
-                return convertView;
-            }
-        };
-
-        infiniteListView.setAdapter(adapter);
+        infiniteListView = (RecyclerView) findViewById(R.id.content_chat);
+        infiniteListView.setHasFixedSize(true);
+        infiniteListView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        RecyclerView.Adapter chatAdapter = new ChatAdapter(itemList, this);
+        infiniteListView.swapAdapter(chatAdapter, false);
         getReportes();
     }
 
     void getReportes() {
-        infiniteListView.startLoading();
+        //infiniteListView.startLoading();
         String url = "http://104.236.33.228:8050/reportes/respuesta/list/?reporte=" + reporte;
         JsonObjectRequest reportesRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 try {
-                    infiniteListView.stopLoading();
+                    //infiniteListView.stopLoading();
                     JSONArray object_list = response.getJSONArray("object_list");
                     for (int i = 0; i < object_list.length(); i++) {
                         JSONObject campo = object_list.getJSONObject(i);
@@ -153,9 +66,10 @@ public class ChatActivity extends AppCompatActivity {
                         String mensaje = campo.getString("mensaje");
                         String user = campo.getString("user");
                         boolean tu = campo.getBoolean("tu");
-                        infiniteListView.addNewItem(new Mensaje(fecha, mensaje, tu, user));
+                        itemList.add(new Mensaje(fecha, mensaje, tu, user));
                     }
-                    infiniteListView.hasMore(false);
+                    RecyclerView.Adapter chatAdapter = new ChatAdapter(itemList, ChatActivity.this);
+                    infiniteListView.swapAdapter(chatAdapter, false);
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
---
+++ app/src/main/java/co/com/exile/piscix/ChatAdapter.java
@@ -0,0 +1,101 @@
+package co.com.exile.piscix;
+
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+import co.com.exile.piscix.models.Mensaje;
+
+
+public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ContactoViewHolder> {
+
+    private List<Mensaje> mensajes;
+    private Context context;
+
+
+    ChatAdapter(List<Mensaje> contactos, Context context) {
+        this.mensajes = contactos;
+        this.context = context;
+    }
+
+    static class ContactoViewHolder extends RecyclerView.ViewHolder {
+        TextView mensaje;
+        TextView user;
+        CardView bubble;
+
+
+        ContactoViewHolder(View itemView) {
+            super(itemView);
+
+            user = (TextView) itemView.findViewById(R.id.user);
+            mensaje = (TextView) itemView.findViewById(R.id.message);
+            bubble = (CardView) itemView.findViewById(R.id.bubble);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mensajes.size();
+    }
+
+    @Override
+    public ContactoViewHolder onCreateViewHolder(ViewGroup viewGroup, final int i) {
+        final View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.chat_bubble, viewGroup, false);
+        return new ContactoViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ContactoViewHolder contactoViewHolder, int i) {
+        final int position = i;
+        Mensaje mensaje = mensajes.get(i);
+
+        float scale = context.getApplicationContext().getResources().getDisplayMetrics().density;
+        final int max_margin = (int) (72 * scale + 0.5f);
+        final int min_margin = (int) (16 * scale + 0.5f);
+        int top_margin = (int) (16 * scale + 0.5f);
+        int bottom_margin = 0;
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        if (position > 0 && mensajes.get(position - 1).getUser().equals(mensaje.getUser())) {
+            top_margin = (int) (2 * scale + 0.5f);
+        }
+
+        if ((position + 1) >= mensajes.size()) {
+            bottom_margin = (int) (16 * scale + 0.5f);
+        }
+
+        contactoViewHolder.mensaje.setText(mensaje.getMensaje());
+        if (!mensaje.isTu()) {
+            contactoViewHolder.mensaje.setTextColor(ContextCompat.getColor(context, R.color.white));
+            contactoViewHolder.user.setText(mensaje.getUser());
+            if (position > 0 && mensajes.get(position - 1).getUser().equals(mensaje.getUser())) {
+                contactoViewHolder.user.setVisibility(View.GONE);
+            } else {
+                contactoViewHolder.user.setVisibility(View.VISIBLE);
+            }
+            contactoViewHolder.bubble.setCardBackgroundColor(ContextCompat.getColor(context, R.color.colorPrimary));
+            layoutParams.setMargins(min_margin, top_margin, max_margin, bottom_margin);
+        } else {
+            contactoViewHolder.user.setVisibility(View.GONE);
+            contactoViewHolder.bubble.setCardBackgroundColor(ContextCompat.getColor(context, R.color.white));
+            contactoViewHolder.mensaje.setTextColor(ContextCompat.getColor(context, R.color.grey));
+            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_END);
+            layoutParams.setMargins(max_margin, top_margin, min_margin, bottom_margin);
+        }
+        contactoViewHolder.bubble.setLayoutParams(layoutParams);
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+}
