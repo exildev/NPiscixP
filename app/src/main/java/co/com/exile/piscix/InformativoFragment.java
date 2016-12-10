de9eca9331707690fdf16e6af289339b284064e4
Yamid Pico <ypicoleal@gmail.com>
2016-12-10 16:33:20 -0500

    mensaje de no network al usuario

--- app/src/main/java/co/com/exile/piscix/InformativoFragment.java
+++ app/src/main/java/co/com/exile/piscix/InformativoFragment.java
@@ -15,6 +15,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
@@ -290,7 +291,13 @@ public class InformativoFragment extends Fragment implements GoogleApiClient.Con
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         loading.dismiss();
-                        Log.e("solucion", new String(error.networkResponse.data));
+                        if (error.networkResponse == null) {
+                            View view = getView();
+                            assert view != null;
+                            Snackbar.make(view, R.string.no_network, Snackbar.LENGTH_LONG).show();
+                        } else {
+                            Log.e("solucion", new String(error.networkResponse.data));
+                        }
                     }
                 }){
             @Override
